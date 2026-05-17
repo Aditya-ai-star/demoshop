67c6425586affc80e1c5aac9fbff13c0482ecac9
Aditya <rajaditya21677@gmail.com>
2026-05-17 09:42:19 +0530

    add_to_cart

---
+++ features/add_to_cart.feature
@@ -0,0 +1,6 @@
+Feature: Add Product To Cart
+
+  Scenario: Add product to cart successfully
+    Given user opens homepage
+    When user adds product to cart
+    Then product should be added successfully
---
+++ step_definitions/test_cart_steps.py
@@ -0,0 +1,24 @@
+from pytest_bdd import scenarios, given, when, then
+from pages.cart_page import CartPage
+
+
+scenarios("../features/add_to_cart.feature")
+
+
+@given("user opens homepage")
+def open_homepage(browser):
+    cart = CartPage(browser)
+    cart.open_homepage()
+
+
+@when("user adds product to cart")
+def add_product(browser):
+    cart = CartPage(browser)
+    cart.add_product_to_cart()
+
+
+@then("product should be added successfully")
+def verify_cart(browser):
+    cart = CartPage(browser)
+
+    assert cart.verify_success_message()
